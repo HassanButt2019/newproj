02411208b910082b43c19b0b9f1966bef17ea47d
hbutt <48251967+HassanButt2019@users.noreply.github.com>
2024-03-14 15:20:40 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Button from '$lib/Button.svelte';
+    function handleClick() {
+        console.log('clicked');
+    }
+</script>
+
+<Button on:click={handleClick}>My Button</Button>
